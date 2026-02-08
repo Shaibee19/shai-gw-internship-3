263401dda1c9d4b0325a5863409469f434478a2f
Shaibee19 <shaipattzgray@gmail.com>
2026-02-08 16:26:42 -0500

    finished TopSellers Component

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -101,6 +101,7 @@ const NewItems = () => {
   }
   fetchData();
   }, []);
+
   
   return (
     <section id="section-items" className="no-bottom">
@@ -138,7 +139,7 @@ const NewItems = () => {
                       <div className="nft__item">
                         <div className="author_list_pp">
                           <Link
-                            to={`/${item.authorId}`}
+                            to={`/author/${item.authorId}`}
                             data-bs-toggle="tooltip"
                             data-bs-placement="top"
                             title={`Creator: ${item.authorId}`}
@@ -169,7 +170,7 @@ const NewItems = () => {
                             </div>
                           </div>
 
-                          <Link to={`/${item.nftId}`}>
+                          <Link to={`/nftId/${item.nftId}`}>
                             <img
                               src={item.nftImage}
                               className="lazy nft__item_preview"
@@ -178,10 +179,10 @@ const NewItems = () => {
                           </Link>
                         </div>
                         <div className="nft__item_info">
-                          <Link to={`/${item.nftId}`}>
+                          <Link to={`/nftId/${item.nftId}`}>
                             <h4>{item.title}</h4>
                           </Link>
-                          <div className="nft__item_price">${item.price}</div>
+                          <div className="nft__item_price">{item.price} ETH</div>
                           <div className="nft__item_like">
                             <i className="fa fa-heart"></i>
                             <span>{item.likes}</span>
